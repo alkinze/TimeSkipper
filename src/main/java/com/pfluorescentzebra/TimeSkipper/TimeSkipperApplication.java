e691d08b8d3784839ea8b36d263ebdbe8fedbdd5
Stephanie Polansky <alkinze@gmail.com>
2020-07-13 11:59:22 -0500

    initial commit

---
+++ src/main/java/com/pfluorescentzebra/TimeSkipper/TimeSkipperApplication.java
@@ -0,0 +1,13 @@
+package com.pfluorescentzebra.TimeSkipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSkipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSkipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfluorescentzebra/TimeSkipper/TimeSkipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfluorescentzebra.TimeSkipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSkipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
